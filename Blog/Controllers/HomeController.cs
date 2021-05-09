03e45bde8f49e2098e0c60d1a82f8edf5ddd9ef5
Taklemariam Alazar <teklealazar399@gmail.com>
2021-05-09 22:57:25 +0300

    Folder structure Updated

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Post()
+        {
+            return View();
+        }
+
+    }
+}
--- Blog/Startup.cs
+++ Blog/Startup.cs
@@ -16,6 +16,7 @@ namespace Blog
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc(option => option.EnableEndpointRouting = false);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,8 +26,9 @@ namespace Blog
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseMvcWithDefaultRoute();
 
-            app.UseRouting();
+            /*app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
@@ -34,7 +36,7 @@ namespace Blog
                 {
                     await context.Response.WriteAsync("Hello World!");
                 });
-            });
+            });*/
         }
     }
 }
