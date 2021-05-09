6f3dcdfab796e718602c608941031c039a821e0b
Taklemariam Alazar <teklealazar399@gmail.com>
2021-05-09 23:32:29 +0300

    Edit page created and Updated

--- Blog/Controllers/HomeController.cs
+++ Blog/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,16 @@ namespace Blog.Controllers
         public IActionResult Post()
         {
             return View();
+        }
+        [HttpGet]
+        public IActionResult Edit()
+        {
+            return View(new Post());
+        }
+        [HttpPost]
+        public IActionResult Edit(Post post)
+        {
+            return RedirectToAction("Index");
         }
 
     }
---
+++ Blog/Models/Post.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Blog.Models
+{
+    public class Post
+    {
+        public string Title { get; set; } = "";
+        public string Body { get; set; } = "";
+        public DateTime Created { get; set; } = DateTime.Now;
+    }
+}
